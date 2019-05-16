1be15de5403234236f0605c4192e67762bc432ed
Alena Yuryeva <nimfetrisa@gmail.com>
2019-05-16 20:41:32 +0200

    Tests: connects in both cfgs, login passes for plaintext, connection aborted for TLS. Big time changes in some methods -- in need for major refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "encryption")]
 use rustls::{ClientConfig, ClientSession, Session};
-#[cfg(not(feature = "encryption"))]
+use std::cmp::min;
 use std::io::BufRead;
 use std::io::{BufReader, Read, Write};
 use std::net::{SocketAddr, TcpStream};
@@ -13,6 +13,7 @@ pub struct Pop3Client {
     client: BufReader<TcpStream>,
     #[cfg(feature = "encryption")]
     tls: ClientSession,
+    authorized: bool,
 }
 
 type Pop3Result = Result<String, String>;
@@ -23,6 +24,7 @@ impl Pop3Client {
         TcpStream::connect(addr.into())
             .map(|client| Self {
                 client: BufReader::new(client),
+                authorized: false,
             })
             .map(|mut client| {
                 client
@@ -45,25 +47,70 @@ impl Pop3Client {
             .map(|client| Self {
                 client: BufReader::new(client),
                 tls: ClientSession::new(&config, hostname),
+                authorized: false,
             })
             .map(|mut client| {
+                let mut buf = String::new();
                 client
-                    .read_response()
-                    .map_err(|e| eprintln!("{:?}", e))
+                    .client
+                    .read_line(&mut buf)
+                    .map_err(|e| e.to_string())
+                    .and_then(|_| {
+                        if buf.starts_with("+OK") {
+                            Ok(buf[4..].to_owned())
+                        } else {
+                            Err(buf[5..].to_owned())
+                        }
+                    })
+                    .map_err(|e| eprintln!("Connect: {:?}", e))
                     .ok()
                     .map(|_| client)
             })
+            .map(|client| {
+                client.map(|mut client| {
+                    client
+                        .client
+                        .get_mut()
+                        .write_all("STLS\r\n".as_bytes())
+                        .map_err(|e| e.to_string())
+                        .and_then(|_| {
+                            let mut buf = String::new();
+                            client
+                                .client
+                                .read_line(&mut buf)
+                                .map_err(|e| e.to_string())
+                                .and_then(|_| {
+                                    if buf.starts_with("+OK") {
+                                        Ok(buf[4..].to_owned())
+                                    } else {
+                                        Err(buf[5..].to_owned())
+                                    }
+                                })
+                        })
+                        .map_err(|e| eprintln!("Starttls: {:?}", e))
+                        .ok()
+                        .map(|_| client)
+                })
+            })
             .map_err(|e| eprintln!("{:?}", e))
+            .unwrap_or(None)
             .unwrap_or(None)
     }
 
     pub fn login(&mut self, login: impl Into<String>, password: impl Into<String>) -> Pop3Result {
+        if self.authorized {
+            return Err("login is only allowed in Authorization stage".to_string());
+        }
         let login_query = format!("USER {}\r\n", login.into());
         let password_query = format!("PASS {}\r\n", password.into());
 
         self.send(login_query, false).and_then(|s1| {
             self.send(password_query, false)
                 .map(|s2| format!("{}{}", s1, s2))
+                .map(|s| {
+                    self.authorized = true;
+                    s
+                })
         })
     }
 
@@ -121,8 +168,14 @@ impl Pop3Client {
     }
 
     pub fn apop(&mut self, name: String, digest: String) -> Pop3Result {
+        if self.authorized {
+            return Err("login is only allowed in Authorization stage".to_string());
+        }
         let query = format!("APOP {} {}\r\n", name, digest);
-        self.send(query, false)
+        self.send(query, false).map(|s| {
+            self.authorized = true;
+            s
+        })
     }
 
     #[cfg(not(feature = "encryption"))]
@@ -132,11 +185,22 @@ impl Pop3Client {
         self.client
             .read_line(&mut buffer)
             .map_err(|e| e.to_string())
+            .and_then(|x| {
+                if x == 0 {
+                    Err("Connection aborted".to_string())
+                } else {
+                    Ok(x)
+                }
+            })
             .and_then(|_| {
                 if buffer.starts_with("+OK") {
                     Ok(buffer[4..].to_owned())
                 } else {
-                    Err(buffer[5..].to_owned())
+                    Err(if buffer.len() < 6 {
+                        buffer.clone()
+                    } else {
+                        buffer[5..].to_owned()
+                    })
                 }
             })
             .and_then(|s| {
@@ -159,6 +223,7 @@ impl Pop3Client {
                         if read.is_err() {
                             return read;
                         }
+                        eprintln!("Buffer content: {}", buffer);
                         response.push_str(&buffer);
                     }
                     Ok(response)
@@ -171,7 +236,7 @@ impl Pop3Client {
     #[cfg(feature = "encryption")]
     fn read_response(&mut self) -> Pop3Result {
         let mut response = String::new();
-        let mut buffer = String::new();
+        let mut buffer = Vec::new();
         while self.tls.wants_read() {
             let res = self
                 .tls
@@ -180,7 +245,7 @@ impl Pop3Client {
                 .and_then(|_| self.tls.process_new_packets().map_err(|e| e.to_string()))
                 .and_then(|_| {
                     self.tls
-                        .read_to_string(&mut buffer)
+                        .read_to_end(&mut buffer)
                         .map_err(|e| e.to_string())
                         .and_then(|x| {
                             if x == 0 {
@@ -189,12 +254,13 @@ impl Pop3Client {
                                 Ok(x)
                             }
                         })
+                        .and_then(|_| String::from_utf8(buffer.clone()).map_err(|e| e.to_string()))
                 })
-                .map(|_| response.push_str(&buffer))
+                .map(|s| response.push_str(&s))
                 .map(|_| buffer.clear())
                 .map(|_| String::new());
             if res.is_err() {
-                eprintln!("{}", response);
+                eprintln!("Err reading TLS: {}", response);
                 return res;
             }
         }
@@ -213,6 +279,10 @@ impl Pop3Client {
             .write_all(query.as_bytes())
             .map_err(|e| e.to_string())
             .and_then(|_| self.read_response(multiline))
+            .map_err(|e| {
+                eprintln!("Error: {}", e);
+                e
+            })
     }
 
     #[cfg(feature = "encryption")]
@@ -279,9 +349,54 @@ mod tests {
         assert!(connect().is_some());
     }
 
+    #[test]
+    fn login_success() {
+        let mut client = connect().unwrap();
+        let result = client.login("e913202b66b623", "1ddf1a9bd7fc45");
+        eprintln!("login_success: {:?}", result);
+        assert!(result.is_ok())
+    }
+
+    #[test]
+    fn login_wrong_login() {
+        let mut client = connect().unwrap();
+        let result = client.login("e913202b66b62", "1ddf1a9bd7fc45");
+        eprintln!("wrong_login: {:?}", result);
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn login_wrong_password() {
+        let mut client = connect().unwrap();
+        let result = client.login("e913202b66b623", "1ddf1a9bd7fc4");
+        eprintln!("wrong_password: {:?}", result);
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn login_wrong_stage() {
+        let mut client = connect().unwrap();
+        client.login("e913202b66b623", "1ddf1a9bd7fc45").ok();
+        let result = client.login("e913202b66b623", "1ddf1a9bd7fc45");
+        eprintln!("login_wrong_stage: {:?}", result);
+        assert!(result.is_err())
+    }
+
+    // This test will fail if the server implementation does not comply to specification
+    #[cfg(false)]
+    #[test]
+    fn login_already_locked() {
+        connect()
+            .unwrap()
+            .login("e913202b66b623", "1ddf1a9bd7fc45")
+            .ok();
+        let mut client = connect().unwrap();
+        let result = client.login("e913202b66b623", "1ddf1a9bd7fc45");
+        eprintln!("login_already_locked: {:?}", result);
+        assert!(result.is_err())
+    }
+
     // TODO
-    // test connect
-    // test login (succ, wrong login, wrong pass, wrong stage, already locked)
     // test apop (succ, wrong login, wrong digest, wrong stage, already locked)
     // test quit
     // test stat (succ, wrong stage)
